069d24aabda2b70427c4e149e57091d17d30cb92
Pranshu Kashyap <pranshu.kashyap.mec19@itbhu.ac.in>
2021-12-24 19:36:52 +0530

    Add Coin Styles

--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -1,17 +1,17 @@
 import styles from "./Coin.module.css";
 
 const Coin = (props) => {
-    const { icon, coinName, coinSymbol, price, marketCap, priceChange, id } = props;
+    const { icon, coinName, coinSymbol, price, marketCap, priceChange } = props;
     const priceChangeClass = `${styles["price-change"]} ${priceChange < 0 ? styles.red : styles.green}`;
     return (
         <div className={styles.container}>
             <div className={styles.coin}>
                 <img src={icon} alt="name" />
-                <h1 className="coinName">{coinName}</h1>
-                <p className="coinSymbol">{coinSymbol}</p>
-                <p className="coinPrice">$ {price.toFixed(2)}</p>
+                <h1 className={styles.coinName}>{coinName}</h1>
+                <p className={styles.coinSymbol}>{coinSymbol}</p>
+                <p className={styles.coinPrice}>$ {price.toFixed(2)}</p>
                 <p className={priceChangeClass}>{priceChange.toFixed(2)} %</p>
-                <p className="coinVolume">$ {marketCap.toLocaleString()}</p>
+                <p className={styles.coinVolume}>$ {marketCap.toLocaleString()}</p>
                 <button>More Info</button>
             </div>
         </div>
